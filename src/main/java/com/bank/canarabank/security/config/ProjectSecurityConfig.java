cb96197bef7ba8b2c58b4c7ad5816d354acfb0e2
Yogesh Ukale <yogeshukale07@gmail.com>
2026-01-10 17:53:58 +0530

    #3- Added InMemory Security

---
+++ src/main/java/com/bank/canarabank/security/config/ProjectSecurityConfig.java
@@ -0,0 +1,50 @@
+package com.bank.canarabank.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.password.CompromisedPasswordChecker;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class ProjectSecurityConfig {
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) {
+        http.authorizeHttpRequests(req -> req.requestMatchers("/myAccount", "/myBalance", "/myLoans", "/myCards").authenticated()
+                .requestMatchers("/contact", "/notices").permitAll());
+
+        http.formLogin(withDefaults());
+
+        http.httpBasic(withDefaults());
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService loadUserByUsername() {
+        UserDetails defaultUserDetails = User.withUsername("canara").password("{noop}canara").authorities("read").build();
+        UserDetails sonaliUserDetails = User.withUsername("Sonali").password("{bcrypt}$2a$12$cVsMMarsR/.sRe35Feluw.YOZhABn1rji5Z.FDCZQJKkZMZprCEGu").authorities("read").build();
+        UserDetails yogeshUserDetails = User.withUsername("Yogesh").password("{bcrypt}$2a$12$cVauph61oWwtaQufaJPwousFaH/rss5Z.WLs.bZinmdxFlLhe0Mji").authorities("read").build();
+        return new InMemoryUserDetailsManager(defaultUserDetails, sonaliUserDetails, yogeshUserDetails);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    //@Bean
+    public CompromisedPasswordChecker compromisedPasswordChecker() {
+        return new HaveIBeenPwnedRestApiPasswordChecker();
+    }
+}
